e7d637bb31e8ff13d718d2c7b4227a5efa363a3f
Bakary Diarra <71572448+diarrabak@users.noreply.github.com>
2021-04-05 11:18:59 -0400

    User table too
    I also added user table, modify it if necessary

--- SAH/App_Start/WebApiConfig.cs
+++ SAH/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace SAH
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}//{action}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
