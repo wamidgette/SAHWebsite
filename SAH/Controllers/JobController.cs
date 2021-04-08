f2a7865f998c1b2aaa22a15c93699ef17c033ee7
Barbara Cam <cam.barbara@hotmail.com>
2021-04-08 18:21:16 -0400

    barbara2

--- SAH/Controllers/JobController.cs
+++ SAH/Controllers/JobController.cs
@@ -3,21 +3,85 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Diagnostics;
+using System.Web.Script.Serialization;
+using SAH.Models;
+using SAH.Models.ModelViews;
 
 namespace SAH.Controllers
 {
     public class JobController : Controller
     {
-        // GET: Job
-        public ActionResult Index()
+
+        private JavaScriptSerializer jss = new JavaScriptSerializer();
+        private static readonly HttpClient client;
+
+
+        static JobController()
         {
-            return View();
+            HttpClientHandler handler = new HttpClientHandler()
+            {
+                AllowAutoRedirect = false
+            };
+            client = new HttpClient(handler);
+            //change this to match your own local port number
+            client.BaseAddress = new Uri("https://localhost:44378/api/");
+            client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+
+            //client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", ACCESS_TOKEN);
+
+        }
+
+        // GET: Job/List
+        public ActionResult List()
+        {
+            string url = "JobData/GetJobs";
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                IEnumerable<JobDto> SelectedJobs = response.Content.ReadAsAsync<IEnumerable<JobDto>>().Result;
+                return View(SelectedJobs);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
         }
 
         // GET: Job/Details/5
-        public ActionResult Details(int id)
+        public ActionResult Details(int Id)
         {
-            return View();
+            //Model used to combine a Parking Spot object and its tickets
+            ShowJob ViewModel = new ShowJob();
+
+            //Get the current ParkingSpot object
+            string url = "JobData/FindJob/" + Id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                JobDto SelectedJob = response.Content.ReadAsAsync<JobDto>().Result;
+                ViewModel.Job = SelectedJob;
+
+                //No an error because a Job not having any applicants is not a problem
+                url = "JobData/GetJobApplications/" + Id;
+                response = client.GetAsync(url).Result;
+                //Can catch the status code (200 OK, 301 REDIRECT), etc.
+                //Debug.WriteLine(response.StatusCode);
+                IEnumerable<ApplicationDto> SelectedApplications = response.Content.ReadAsAsync<IEnumerable<ApplicationDto>>().Result;
+                ViewModel.Applications = SelectedApplications;
+
+                return View(ViewModel);
+
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
+
         }
 
         // GET: Job/Create
@@ -28,62 +92,116 @@ namespace SAH.Controllers
 
         // POST: Job/Create
         [HttpPost]
-        public ActionResult Create(FormCollection collection)
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Job JobInfo)
         {
-            try
-            {
-                // TODO: Add insert logic here
+            Debug.WriteLine(JobInfo.Position);
+            string url = "Jobdata/AddJob";
+            Debug.WriteLine(jss.Serialize(JobInfo));
+            HttpContent content = new StringContent(jss.Serialize(JobInfo));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
 
-                return RedirectToAction("Index");
-            }
-            catch
+            if (response.IsSuccessStatusCode)
             {
-                return View();
+
+                int JobId = response.Content.ReadAsAsync<int>().Result;
+                return RedirectToAction("Details", new { Id = JobId });
+            }
+            else
+            {
+                return RedirectToAction("Error");
             }
         }
 
         // GET: Job/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit(int Id)
         {
-            return View();
+            string url = "Jobdata/FindJob/" + Id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            //Can catch the status code (200 OK, 301 REDIRECT), etc.
+            //Debug.WriteLine(response.StatusCode);
+            if (response.IsSuccessStatusCode)
+            {
+                //Put data into Job data transfer object
+                JobDto SelectedJob = response.Content.ReadAsAsync<JobDto>().Result;
+                return View(SelectedJob);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
         }
 
         // POST: Job/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        [ValidateAntiForgeryToken()]
+        public ActionResult Edit(int Id, Job JobInfo)
         {
-            try
-            {
-                // TODO: Add update logic here
+            Debug.WriteLine(JobInfo.Position);
+            string url = "Jobdata/UpdateJob/" + Id;
+            Debug.WriteLine(jss.Serialize(JobInfo));
+            HttpContent content = new StringContent(jss.Serialize(JobInfo));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
 
-                return RedirectToAction("Index");
-            }
-            catch
+            if (response.IsSuccessStatusCode)
             {
-                return View();
+                return RedirectToAction("Details", new { Id = Id });
+            }
+            else
+            {
+                return RedirectToAction("Error");
             }
         }
 
         // GET: Job/Delete/5
-        public ActionResult Delete(int id)
+        [HttpGet]
+        public ActionResult DeleteConfirm(int Id)
         {
-            return View();
+            string url = "JobData/FindJob/" + Id;
+            HttpResponseMessage response = client.GetAsync(url).Result;
+            //Can catch the status code (200 OK, 301 REDIRECT), etc.
+            //Debug.WriteLine(response.StatusCode);
+            if (response.IsSuccessStatusCode)
+            {
+                //Put data into Job data transfer object
+                JobDto SelectedJob = response.Content.ReadAsAsync<JobDto>().Result;
+                return View(SelectedJob);
+            }
+            else
+            {
+                return RedirectToAction("Error");
+            }
         }
 
         // POST: Job/Delete/5
         [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
+        [ValidateAntiForgeryToken()]
+        public ActionResult Delete(int Id)
         {
-            try
+            string url = "Jobdata/DeleteJob/" + Id;
+            //post body is empty
+            HttpContent content = new StringContent("");
+            HttpResponseMessage response = client.PostAsync(url, content).Result;
+            //Can catch the status code (200 OK, 301 REDIRECT), etc.
+            //Debug.WriteLine(response.StatusCode);
+            if (response.IsSuccessStatusCode)
             {
-                // TODO: Add delete logic here
 
-                return RedirectToAction("Index");
+                return RedirectToAction("List");
             }
-            catch
+            else
             {
-                return View();
+                return RedirectToAction("Error");
             }
         }
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+
     }
 }
+
---
+++ SAH/Models/ModelViews/ShowJob.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SAH.Models.ModelViews
+{
+    public class ShowJob
+    {
+        //Information about the job
+        public JobDto Job { get; set; }
+
+        //Information about all application for the Job
+        public IEnumerable<ApplicationDto> Applications { get; set; }
+
+    }
+}
