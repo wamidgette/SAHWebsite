c13da3e0adaadb7845698e06cbcd99091fac1df8
Barbara Cam <cam.barbara@hotmail.com>
2021-04-22 17:29:22 -0400

    update for application is working

---
+++ SAH/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace SAH.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
