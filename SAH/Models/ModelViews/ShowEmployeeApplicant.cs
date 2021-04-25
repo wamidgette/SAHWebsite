63aa93a3e8ef0fd076f2b37d5cf1df7790674541
Danyal <71612770+danyaleffendi@users.noreply.github.com>
2021-04-25 13:21:23 -0400

    updated userdto

--- SAH/Models/ModelViews/ShowEmployeeApplicant.cs
+++ SAH/Models/ModelViews/ShowEmployeeApplicant.cs
@@ -10,7 +10,7 @@ namespace SAH.Models.ModelViews
         //Information about the Courses
         public CoursesDto Courses { get; set; }
         //Information about the Courses of the application
-        public UserDto User { get; set; }
+        public ApplicationUserDto User { get; set; }
         //Information about the Application
         public EmployeeApplicantDto EmployeeApplicant { get; set; }
         //Information about the Role of User
